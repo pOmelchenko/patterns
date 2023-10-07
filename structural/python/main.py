9065322a8b3f729c723824f6946be141f503ee22
Pavel Omelchenko <p.Omelchenko@gmail.com>
2023-10-07 15:42:10 +0300

    python: fixes and updates of examples

--- structural/python/main.py
+++ structural/python/main.py
@@ -12,13 +12,14 @@ def dec_without_args(function):
 
 @dec_without_args
 def with_dec_which_has_not_args():
-    pass
+    print('call with_dec_which_has_not_args')
 
 
 def dec_with_args(message: str):
     def wrapper(function):
         def dec():
             print(f'"{message}" from decorator with args')
+            function()
 
         return dec
     return wrapper
@@ -26,15 +27,27 @@ def dec_with_args(message: str):
 
 @dec_with_args('some message')
 def with_dec_which_has_args():
-    pass
+    print('call with_dec_which_has_args')
+
+
+@dec_with_args('first')
+@dec_with_args('second')
+def with_two_decs():
+    print('call with_two_decs')
 
 
 def main():
     print_hello_world()
+    print()
 
     with_dec_which_has_not_args()
+    print()
 
     with_dec_which_has_args()
+    print()
+
+    with_two_decs()
+    print()
 
 
 if __name__ == '__main__':
